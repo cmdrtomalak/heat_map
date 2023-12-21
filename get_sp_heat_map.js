a8c1ed34aea0340e864d0bd22eab2ff6fda1112e
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2023-12-21 11:27:27 -0500

    Add 10yr yield

--- get_sp_heat_map.js
+++ get_sp_heat_map.js
@@ -52,17 +52,51 @@ async function captureHeatMap(url) {
 	}
 }
 
-async function sendLatestImage(chart_url, webhookUrl) {
-	// Get screenshot
-	let screenshotBase64 = await captureHeatMap(chart_url);
+async function capture10YrYield(url) {
+	let options = new chrome.Options();
+	options.addArguments('--headless');
+	options.addArguments('--disable-gpu');
+	options.addArguments('user-agent=Chrome/88.0.4324.150');
+	options.windowSize({ width: 1000, height: 800 });
 
-	if (!screenshotBase64) {
-		logger.error('Failed to capture screenshot.');
+	driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
+
+	try {
+		await driver.get(url);
+
+		// await driver.executeScript("window.scrollBy(0, 150)");
+
+		let canvasElement = await driver.findElement(By.id('main-3-FullScreenChartIQ-Proxy'));
+
+		let screenshot = await canvasElement.takeScreenshot();
+
+		// fs.writeFileSync('images/1.png', screenshot, 'base64');
+		return screenshot;
+	}
+	finally {
+		await driver.quit();
+	}
+}
+
+
+async function sendLatestImages(sp_500_chart_url, yieldurl, webhookUrl) {
+	// Get screenshot
+	let sp_500_screenshotBase64 = await captureHeatMap(sp_500_chart_url);
+	let yield_chart = await capture10YrYield(yieldurl);
+
+	if (!sp_500_screenshotBase64) {
+		logger.error('Failed to capture S&P 500 screenshot.');
 		return;
 	}
 
-	// Prepare the payload
-	const screenshotBuffer = Buffer.from(screenshotBase64, 'base64')
+	if (!yield_chart) {
+		logger.error('Failed to capture 10Y Yield screenshot.');
+		return;
+	}
+
+	// Prepare the payload of S&P
+	const screenshotBuffer = Buffer.from(sp_500_screenshotBase64, 'base64')
+	logger.info('Screenshot buffer is ' + screenshotBuffer.length + ' long.')
 	if (screenshotBuffer.length < 100) {
 		logger.error('Screenshot buffer is too small, likely invalid.');
 		return;
@@ -75,15 +109,38 @@ async function sendLatestImage(chart_url, webhookUrl) {
 		const response = await axios.post(webhookUrl, formData, {
 			headers: formData.getHeaders()
 		});
-		logger.info('File sent successfully:', response.statusText);
+		logger.info('S&P File sent successfully:', response.statusText);
 	} catch (error) {
-		logger.error('Error sending file:', error.message);
+		logger.error('Error sending S&P file:', error.message);
+	}
+
+	// Prepare the payload of 10 Year Yield
+	const screenshotBuffer2 = Buffer.from(yield_chart, 'base64')
+	logger.info('Screenshot buffer is ' + screenshotBuffer2.length + ' long.')
+	if (screenshotBuffer2.length < 100) {
+		logger.error('Screenshot buffer is too small, likely invalid.');
+		return;
+	}
+
+	const formData2 = new FormData();
+	formData2.append('file', screenshotBuffer2, { filename: 'screenshot.png' });
+
+	try {
+		const response = await axios.post(webhookUrl, formData2, {
+			headers: formData2.getHeaders()
+		});
+		logger.info('10 Year Yield File sent successfully:', response.statusText);
+	} catch (error) {
+		logger.error('Error sending 10 Year Yield file:', error.message);
 	}
 }
 
 webhook_url = process.env.WEBHOOK_URL;
+logger.info('Webhook: ' + process.env.WEBHOOK_URL);
 
-sendLatestImage('https://finviz.com/map.ashx?t=sec', webhook_url)
+
+let YFinance10YrYieldUrl = 'https://finance.yahoo.com/chart/%5ETNX';
+sendLatestImages('https://finviz.com/map.ashx?t=sec', YFinance10YrYieldUrl, webhook_url)
 .then(() => {
     logger.info('Operation completed, exiting script.');
     process.exit(0); // Exits the process successfully
@@ -92,3 +149,4 @@ sendLatestImage('https://finviz.com/map.ashx?t=sec', webhook_url)
     logger.error('An error occurred:', error);
     process.exit(1); // Exits the process with an error code
 });
+
